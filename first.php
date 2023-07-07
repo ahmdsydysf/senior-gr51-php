f2b27ae97779d0a87f213c984137f9ef594614a1
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-07-07 15:05:15 +0300

    start with date

--- first.php
+++ first.php
@@ -6,7 +6,9 @@
 // 4- result
 
 $myConn = mysqli_connect('localhost', 'root', '', 'hr_db');
-$statme = "SELECT * FROM employees WHERE employee_id > 100 AND employee_id < 120";
+$statme = "SELECT * FROM `employees` where first_name = 'amr'; ";
+// $statme = "SELECT * FROM `employees` where first_name like '%e%e%' ";
+// $statme = "SELECT * FROM employees WHERE employee_id > 100 AND employee_id < 120";
 
 $employess = mysqli_query($myConn, $statme);
 // $final_emps = mysqli_fetch_assoc($employess);
@@ -17,9 +19,9 @@ $erooo = mysqli_error($myConn);
 
 // var_dump($final_emps1);
 
-while($emp = mysqli_fetch_assoc($employess)) {
-    var_dump($emp);
-}
+// while($emp = mysqli_fetch_assoc($employess)) {
+//     var_dump($emp);
+// }
 ?>
 
 <!DOCTYPE html>
@@ -31,14 +33,16 @@ while($emp = mysqli_fetch_assoc($employess)) {
 
 </head>
 <body>
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">firstname</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">email</h6>
-    <p class="card-text">phonenumber</p>
-    <a href="#" class="card-link">salary</a>
-    <a href="#" class="card-link">id : </a>
-  </div>
-</div>
+  <?php while($row = mysqli_fetch_assoc($employess)): ?>
+    <div class="card" style="width: 18rem;background-color:greenyellow;margin:15px">
+      <div class="card-body">
+        <h5 class="card-title"><?= $row['first_name'] ?></h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">email</h6>
+        <p class="card-text">phonenumber</p>
+        <a href="#" class="card-link">salary</a>
+        <a href="#" class="card-link">id : <?= $row['employee_id'] ?> </a>
+      </div>
+    </div>
+<?php endwhile ;?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -24,3 +24,30 @@
 // SELECT * FROM `employees` where employee_id > 125 and employee_id < 150 OR first_name = 'Adam';
 // SELECT first_name , last_name , department_id FROM `employees` WHERE `department_id` in (30 , 90);
 // SELECT first_name , last_name , department_id FROM `employees` WHERE `department_id` BETWEEN 30 and 90;
+
+// SELECT * FROM `employees` where first_name like 'amr';
+// SELECT * FROM `employees` where first_name like '%d';
+// SELECT * FROM `employees` where first_name like '%d%';
+// SELECT * FROM `employees` where first_name like 'd%';
+// SELECT * FROM `employees` where first_name like 'a%d';
+// SELECT * FROM `employees` where first_name like '%e_';
+// SELECT * FROM `employees` where first_name like '%e%e%';
+
+
+// SELECT * FROM `employees` where phone_number is Null;
+// SELECT * FROM `employees` where phone_number is not Null;
+
+// upper , sum , min , max , number , date agrgation functions
+// SELECT last_name , trim(last_name ) FROM `employees` WHERE employee_id = 101;
+// SELECT last_name , trim( Both from last_name ) FROM `employees` WHERE employee_id = 101;
+// SELECT last_name , trim( LEADING from last_name ) FROM `employees` WHERE employee_id = 101;
+// SELECT last_name , trim( TRAILING from last_name ) FROM `employees` WHERE employee_id = 101;
+// SELECT last_name , trim(last_name) , trim('$' from last_name ) FROM `employees`;
+
+
+// SELECT salary , ceil(salary) FROM `employees`;
+// SELECT salary , floor(salary) FROM `employees`;
+// SELECT salary , round(salary) FROM `employees`;
+// SELECT salary , TRUNCATE(salary , 1) FROM `employees`;
+// SELECT salary , TRUNCATE(salary , 2) FROM `employees`;
+// SELECT salary , TRUNCATE(salary , 0) FROM `employees`;
